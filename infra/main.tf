1e06ea3793bd9a3428c684b4c294be725b9bff7a
billmetangmo <25366207+billmetangmo@users.noreply.github.com>
2022-08-12 11:56:36 +0200

    refactor(meeting-notes): switched from GitHub to a jekyll blog (#17)
    Allows searching notes using tags or full text + better UX

--- infra/main.tf
+++ infra/main.tf
@@ -1,37 +1,33 @@
-resource "aws_s3_bucket" "website" {
-  bucket = var.website_bucket_name
-  acl    = "public-read"
+module "s3-website" {
+  source  = "cloudposse/s3-website/aws"
+  version = "0.17.0"
+  # insert the 13 required variables here
 
-  tags = {
-    Name = "Community"
-  }
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST", "GET"]
-    allowed_origins = ["*"]
-  }
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadForGetBucketObjects",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "*"
-      },
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${var.website_bucket_name}/*"
-    }
-  ]
+  hostname  = "blog.mongulu.cm"
+  logs_enabled = false # change namespace from eg to mongulu to avoid bucket already exists
+  parent_zone_name = "mongulu.cm"
 }
-EOF
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+
+module "cdn" {
+  source  = "cloudposse/cloudfront-s3-cdn/aws"
+  version = "0.82.4"
+
+  external_aliases                  = ["blog.mongulu.cm"]
+  dns_alias_enabled                 = true
+  parent_zone_name                  = "mongulu.cm"
+  acm_certificate_arn               = "arn:aws:acm:us-east-1:053932140667:certificate/35f51ab4-1f12-4a13-a96a-b590bbc80b7a"
+  cache_policy_id                   = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad"
+  cloudfront_access_logging_enabled = false
+  s3_access_logging_enabled = false
+  cloudfront_access_log_create_bucket = false
+  website_enabled                   = true
+  origin_bucket = "blog.mongulu.cm"
+  name                          = "lobembe"
+  stage                         = "prod"
+  namespace                     = "mongulu"
+  override_origin_bucket_policy = false
+
+  depends_on = [module.s3-website]
 
 }
